268709faeacc2c4ca3fcf62f879e63906a5232ed
Avijit <avijitroy926avijit@gmail.com>
2026-05-30 02:07:28 +0530

    Update portfolio

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Navbar,Welcome,Dock,Home } from "#components";
+import gsap from "gsap";
+import { Draggable } from "gsap/Draggable";
+import { Finder, Resume, Safari, Terminal, Text, Image, Contact, Photos } from "#windows";
+gsap.registerPlugin(Draggable);
+
+
+const App = () => {
+  return (
+  <main>
+    <Navbar/>
+    <Welcome/>
+    <Dock/>
+    <Terminal/>
+    <Safari/>
+    <Resume/>
+    <Finder/>
+    <Text/>
+    <Image/>
+    <Contact/>
+    <Home/>
+    <Photos/>
+  </main>
+  );
+};
+
+export default App;
